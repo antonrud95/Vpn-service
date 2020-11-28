265094629879a5c61b81b2e303af8c0ed2a92d13
arud95 <antonrud95@yandex.ru>
2020-11-28 14:40:34 +0200

    feature: add hero section

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '~/components/layout'
 import SEO from '~/components/seo.component'
 import HeroSection from '~/components/sections/hero-section/hero-section.component'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Lasles VPN" />
-    <HeroSection />
+    <HeroSection hero={data.datoCmsHero} />
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    datoCmsHero {
+      id
+      description
+      image {
+        fluid {
+          ...GatsbyDatoCmsFluid
+        }
+      }
+    }
+  }
+`
+
 export default IndexPage
