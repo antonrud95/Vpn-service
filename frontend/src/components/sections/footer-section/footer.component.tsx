0f167208b8312309557c28e6905df3cfe14fb643
arud95 <antonrud95@yandex.ru>
2020-11-29 09:00:25 +0200

    feature: add footer hat

--- frontend/src/components/sections/footer-section/footer.component.tsx
+++ frontend/src/components/sections/footer-section/footer.component.tsx
@@ -8,20 +8,18 @@ const FooterSection = () => {
   return (
     <Container fluid className={styles.footerSection}>
       <Container className={styles.footerContainer}>
-        <div className={styles.footerHat}>
-          <div className={styles.footerHatContent}>
-            <h2 className={styles.footerTitle}>
-              Subscribe Now for Get Special Features!
-            </h2>
-            <p className={styles.footerText}>
-              Let's subscribe with us and find the fun.
-            </p>
-          </div>
-          <div className={styles.btnWrapper}>
-            <SButton variant="primary" className={styles.footerBtn}>
-              Subscribe Now
-            </SButton>
-          </div>
+        <div className={styles.footerHatContent}>
+          <h2 className={styles.footerTitle}>
+            Subscribe Now for Get Special Features!
+          </h2>
+          <p className={styles.footerText}>
+            Let's subscribe with us and find the fun.
+          </p>
+        </div>
+        <div className={styles.btnWrapper}>
+          <SButton variant="primary" className={styles.footerBtn}>
+            Subscribe Now
+          </SButton>
         </div>
       </Container>
     </Container>
