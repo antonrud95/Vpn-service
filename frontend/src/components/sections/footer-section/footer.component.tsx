4004308db01880999db2c1ea51f591d7bfc4f430
arud95 <antonrud95@yandex.ru>
2020-11-29 09:49:48 +0200

    wip: work with footer grid

--- frontend/src/components/sections/footer-section/footer.component.tsx
+++ frontend/src/components/sections/footer-section/footer.component.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import SButton from '~/components/ui/general/button/button.component'
 import Logo from '~/components/ui/general/logo/logo.component'
 import { Link } from 'gatsby'
@@ -32,33 +32,97 @@ const FooterSection = () => {
         </div>
       </Container>
       <Container>
-        <div className={styles.containerLinks}>
-          <Logo />
-          <p className={styles.containerText}>
-            <span className={styles.textSpan}>LaslesVPN</span> is a private
-            virtual network that has unique features and has high security.
-          </p>
-          <div className={styles.linksWrapper}>
-            <Link to="/">
-              <div className={styles.linksContent}>
-                <FontAwesomeIcon icon={faFacebookF} className={styles.social} />
+        <Row>
+          <Col xs="12" md="5">
+            <div className={styles.containerLinks}>
+              <Logo />
+              <p className={styles.containerText}>
+                <span className={styles.textSpan}>LaslesVPN</span> is a private
+                virtual network that has unique features and has high security.
+              </p>
+              <div className={styles.linksWrapper}>
+                <Link to="/">
+                  <div className={styles.linksContent}>
+                    <FontAwesomeIcon
+                      icon={faFacebookF}
+                      className={styles.social}
+                    />
+                  </div>
+                </Link>
+                <Link to="/">
+                  <div className={styles.linksContent}>
+                    <FontAwesomeIcon
+                      icon={faTwitter}
+                      className={styles.social}
+                    />
+                  </div>
+                </Link>
+                <Link to="/">
+                  <div className={styles.linksContent}>
+                    <FontAwesomeIcon
+                      icon={faInstagram}
+                      className={styles.social}
+                    />
+                  </div>
+                </Link>
               </div>
+              <p className={styles.footerInfo}>
+                ©2020Lasles<span className={styles.footerInfoSpan}>VPN</span>
+              </p>
+            </div>
+          </Col>
+          <Col xs="6" sm="4" md="2">
+            <p className={styles.footerLinksTitle}>Product</p>
+            <Link to="/" className={styles.footerLinksItem}>
+              Download
             </Link>
-            <Link to="/">
-              <div className={styles.linksContent}>
-                <FontAwesomeIcon icon={faTwitter} className={styles.social} />
-              </div>
+            <Link to="/" className={styles.footerLinksItem}>
+              Pricing
             </Link>
-            <Link to="/">
-              <div className={styles.linksContent}>
-                <FontAwesomeIcon icon={faInstagram} className={styles.social} />
-              </div>
+            <Link to="/" className={styles.footerLinksItem}>
+              Locations
             </Link>
-          </div>
-          <p>
-            ©2020Lasles<span>VPN</span>
-          </p>
-        </div>
+            <Link to="/" className={styles.footerLinksItem}>
+              Server
+            </Link>
+            <Link to="/" className={styles.footerLinksItem}>
+              Countries
+            </Link>
+            <Link to="/" className={styles.footerLinksItem}>
+              Blog
+            </Link>
+          </Col>
+          <Col xs="6" sm="4" md="2">
+            <p className={styles.footerLinksTitle}>Engage</p>
+            <Link to="/" className={styles.footerLinksItem}>
+              LaslesVPN ?
+            </Link>
+            <Link to="/" className={styles.footerLinksItem}>
+              FAQ
+            </Link>
+            <Link to="/" className={styles.footerLinksItem}>
+              Tutorials
+            </Link>
+            <Link to="/" className={styles.footerLinksItem}>
+              About Us
+            </Link>
+            <Link to="/" className={styles.footerLinksItem}>
+              Privacy Policy
+            </Link>
+            <Link to="/" className={styles.footerLinksItem}>
+              Terms of Service
+            </Link>
+          </Col>
+          <Col xs="6" sm="4" md="3">
+            <p className={styles.footerLinksTitle}>Earn money</p>
+            <Link to="/" className={styles.footerLinksItem}>
+              Affiliate
+            </Link>
+            <Link to="/" className={styles.footerLinksItem}>
+              Become Partner
+            </Link>
+          </Col>
+        </Row>
       </Container>
     </Container>
   )
